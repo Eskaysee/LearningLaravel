170b858b7fc71d9628cb2f19cde4af8e0cf478ab
Sihle Calana <sihle.c@finch-technologies.com>
2024-06-20 15:41:13 +0200

    Learnt to use the props (not an HTML attribute) blade directive

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-    <html lang="en">
+    <html lang="en" class="h-full bg-gray-100">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport"
@@ -8,15 +8,7 @@
         <title>firstLarProj</title>
         <script src="https://cdn.tailwindcss.com"></script>
     </head>
-    <body>
-    <!--
-This example requires updating your template:
-
-```
-<html class="h-full bg-gray-100">
-<body class="h-full">
-```
--->
+    <body class="h-full">
         <div class="min-h-full">
             <nav class="bg-gray-800">
                 <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -28,9 +20,9 @@ This example requires updating your template:
                             <div class="hidden md:block">
                                 <div class="ml-10 flex items-baseline space-x-4">
                                     <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                                    <a href="/" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Home</a>
-                                    <a href="/contact" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
-                                    <a href="/about" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+                                    <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
+                                    <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
+                                    <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
                                 </div>
                             </div>
                         </div>
@@ -56,9 +48,9 @@ This example requires updating your template:
                 <div class="md:hidden" id="mobile-menu">
                     <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                        <a href="/" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Home</a>
-                        <a href="/contact" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
-                        <a href="/about" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+                        <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
+                        <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
+                        <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
                     </div>
                     <div class="border-t border-gray-700 pb-3 pt-4">
                         <div class="flex items-center px-5">
