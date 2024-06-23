6efd5a4efe77e5b01b9638c29e77f0a7d1ee58c9
Sihle Calana <sihle.c@finch-technologies.com>
2024-06-23 22:43:58 +0200

    worked a little bit with database migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('full_name');
+            $table->string('email_address')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('secret_password');
             $table->rememberToken();
             $table->timestamps();
         });
