04a1c28a6f46e4d2ff4fc538fa21226b395988cc
Sihle Calana <sihle.c@finch-technologies.com>
2024-06-26 15:39:34 +0200

    Did some Route Model Binding, Created a controller, Used Route View and Route Grouping

---
+++ app/Http/Controllers/JobController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Job;
+
+class JobController extends Controller
+{
+    /**
+     * Display a listing of the Jobs.
+     */
+    public function index() {
+        $jobs = Job::with('employer')->latest()->paginate(5);
+        return view('careers.index', [
+            'jobs' => $jobs
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create() {
+        return view('careers.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request) {
+        $request -> validate([
+            'title' => ['required', 'min:3'],
+            'salary' => ['required']
+        ]);
+
+        Job::create([
+            'title' => request('title'),
+            'salary' => request('salary'),
+            'employer_id' => 1
+        ]);
+        return redirect('/careers');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Job $job) {
+        return view('careers.show', ['job' => $job]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Job $job) {
+        return view('careers.edit', ['job' => $job]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Job $job) {
+        $job->update($request->validate([
+            'title' => ['required', 'min:3'],
+            'salary' => ['required']
+        ]));
+        return redirect('/careers/' . $job->id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Job $job) {
+        $job->delete();
+        return redirect('/careers');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,66 +1,26 @@
 <?php
 
-use App\Models\Job;
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::view('/', 'home');
 
-Route::get('/about', function () {
-    return view('about');
-});
+Route::view('/about','about');
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::view('/contact','contact');
 
-Route::get('/careers', function () {
-    $jobs = Job::with('employer')->latest()->paginate(5);
-//    $jobs = Job::with('employer')->simplePaginate(3);
-//    $jobs = Job::with('employer')->cursorPaginate(3);
-    return view('careers.index', [
-        'jobs' => $jobs
-    ]);
-});
+Route::controller(JobController::class)->group(function () {
+    Route::get('/careers', 'index');
 
-Route::get('/careers/create', function () {
-    return view('careers.create');
-});
+    Route::get('/careers/create', 'create');
 
-Route::get('/careers/{id}', function ($id) {
-    $job = Job::find($id);
-    return view('careers.show', ['job' => $job]);
-});
+    Route::get('/careers/{job}', 'show');
 
-Route::get('/careers/{id}/edit', function ($id) {
-    $job = Job::find($id);
-    return view('careers.edit', ['job' => $job]);
-});
+    Route::get('/careers/{job}/edit', 'edit');
 
-Route::patch('/careers/{job}', function (Job $job) {
-    $job->update(request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required']
-    ]));
-    return redirect('/careers/' . $job->id);
-});
+    Route::patch('/careers/{job}', 'update');
 
-Route::delete('/careers/{job}', function (Job $job) {
-    $job->delete();
-    return redirect('/careers');
-});
+    Route::delete('/careers/{job}', 'destroy');
 
-Route::post('/careers', function () {
-    request() -> validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required']
-    ]);
-
-    Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1
-    ]);
-    return redirect('/careers');
+    Route::post('/careers', 'store');
 });
