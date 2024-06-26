abda215df85b77672e365f4383203b9288b6a090
Sihle Calana <sihle.c@finch-technologies.com>
2024-06-26 12:03:30 +0200

    Created buttons for updating and deleting Jobs. This involved creating PATCH and DELETE requests. Also made use of Route Model Binding

---
+++ resources/views/careers/edit.blade.php
@@ -0,0 +1,51 @@
+<x-layout heading="Edit Job: {{$job -> title}}">
+    <form method="POST" action="/careers/{{$job->id}}">
+        @csrf
+        @method('PATCH')
+        <div class="space-y-12">
+            <div class="border-b border-gray-900/10 pb-12">
+                <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                    <div class="sm:col-span-4">
+                        <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
+                        <div class="mt-2">
+                            <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                <input type="text" name="title" id="title" autocomplete="title"
+                                       class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                       value="{{$job->title}}" placeholder="Software Engineer" required>
+                            </div>
+                            @error('title')
+                                <p class="text-xs text-red-500 font-semibold"> {{ $message }} </p>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="sm:col-span-4">
+                        <label for="salary" class="block text-sm font-medium leading-6 text-gray-900">Salary (R/month)</label>
+                        <div class="mt-2">
+                            <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                <input type="text" name="salary" id="salary"
+                                       class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                       value="{{$job->salary}}" placeholder="35000.00" required>
+                            </div>
+                            @error('salary')
+                            <p class="text-xs text-red-500 font-semibold"> {{ $message }} </p>
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-10">
+{{--                    @if($errors->any())--}}
+{{--                        @foreach($errors->all() as $error)--}}
+{{--                            <li class="text-red-500 italic"> {{ $error }} </li>--}}
+{{--                        @endforeach--}}
+{{--                    @endif--}}
+                </div>
+            </div>
+        </div>
+
+        <div class="mt-6 flex items-center justify-end gap-x-6">
+            <a href="/careers/{{$job->id}}" type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
+            <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+        </div>
+    </form>
+</x-layout>
