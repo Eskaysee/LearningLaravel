4bc83c7a09337d9b74eb4f3bcaa4a32adf8c24f8
Sihle Calana <sihle.c@finch-technologies.com>
2024-07-01 01:00:37 +0200

    used Vite to compile javascript and css for frontend development

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,7 +6,8 @@
               content="width=device-width, initial-scale=1.0">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <title>firstLarProj</title>
-        <script src="https://cdn.tailwindcss.com"></script>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+{{--        <script src="https://cdn.tailwindcss.com"></script>--}}
     </head>
     <body class="h-full">
         <div class="min-h-full">
